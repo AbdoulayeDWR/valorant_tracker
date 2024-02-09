c196771af350b6405c8e7c8607e5f1495bc0b0dd
AbdoulayeDWR <abdoulaye.dwr04@gmail.com>
2024-02-09 16:00:00 +0100

    creation de layout avec header et footer .php

---
+++ layout/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Valorant Tracker</title>
+</head>
+<body>
+    <header>
+        <h1>Valorant Tracker</h1>
+    </header>
+    <nav>
+        <div>
+            <a href="../index.php"><img src="../medias/home.svg" alt="img home svg"></a>
+        </div>
+        <a href="../pages/agent.php">Agents</a>
+        <a href="../pages/maps.php">Cartes</a>
+        <a href="../pages/weapons.php">Armes</a>
+    </nav>
